950fd467f0479dbedd2e3cd6350d0c54c9bfd7f6
MoroXDev <szczepanekrafal27@gmail.com>
2026-01-14 09:01:42 +0100

    dodano plansze

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿char[,] playerBoard = new char[10, 10];
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        playerBoard[i, j] = '~';
+    }
+}
+
+bool isExit = false;
+
+while (!isExit)
+{
+    display();
+    ConsoleKeyInfo key = Console.ReadKey();
+    Console.WriteLine();
+    if (key.KeyChar == 'e')
+        isExit = true;
+}
+void display()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            Console.Write(playerBoard[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
