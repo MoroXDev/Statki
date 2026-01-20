631645e877f649554bba7a89fafb6f5280c66438
Bartosz Adamczyk <Adamczyk.Bartosz@uczen.zselektryk.slupsk.pl>
2026-01-20 23:41:22 +0100

    Dodano kierunki

--- Statki.cs
+++ Statki.cs
@@ -1,6 +1,6 @@
 ﻿class Statki
 {
-    char[,] playerBoard = new char[10, 10];
+    char[,] Gen_board = new char[10, 10];
 
     public Statki()
     {
@@ -8,7 +8,7 @@
         {
             for (int j = 0; j < 10; j++)
             {
-                playerBoard[i, j] = '~';
+                Gen_board[i, j] = '~';
             }
         }
     }
@@ -35,7 +35,7 @@
         {
             for (int j = 0; j < 10; j++)
             {
-                Console.Write(playerBoard[i, j]);
+                Console.Write(Gen_board[i, j]);
             }
             Console.WriteLine();
         }
@@ -47,29 +47,90 @@
         int[] Rand_col = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         Random rand = new Random();
 
-        for (int type = 1; type < 5; type++)
+        for (int ship_type = 1; ship_type < 5; ship_type++)
         {
-            //for (int cell_i = 1; cell_i < 5; cell_i++)
-            //{
-                
-            //}
-            for (int ships_i = 0; ships_i < type; ships_i++)
+            for (int ships_i = 0; ships_i < ship_type; ships_i++)
             {
                 rand.Shuffle(Rand_row);
                 rand.Shuffle(Rand_col);
 
-                //for (int i = 0;i < 10;i++)
-                //{
-                //    for (int j = 0;j < 10;j++)
-                //    {
-                //        if ('~' == playerBoard[Rand_row[i], Rand_col[j]])
-                //        {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if ('~' == Gen_board[Rand_row[i], Rand_col[j]])
+                        {
+                            
+                            int cell_count = 0;
+                            switch (ship_type)
+                            {
+                                case 1:
+                                    cell_count = 4;
+                                    break;
+                                case 2:
+                                    cell_count = 3;
+                                    break;
+                                case 3:
+                                    cell_count = 2;
+                                    break;
+                                case 4:
+                                    cell_count = 1;
+                                    break;
+                            }
+                            for (int dir = 0;dir < 4 ;dir++ )
+                            {
+                                for (int cell_idx = 0; cell_idx < cell_count; cell_idx++)
+                                {
+                                    switch (dir)
+                                    {
+                                        case 1: // LEFT
+                                            if (Gen_board[Rand_row[i] - cell_idx, Rand_col[j]] == '~')
+                                            {
+                                                Gen_board[Rand_row[i] - cell_idx, Rand_col[j]] = 'S';
+                                            }
+                                            else
+                                            {
 
-                //        }
+                                            }
+                                                break;
+                                        case 2: // RIGHT
+                                            if (Gen_board[Rand_row[i] + cell_idx, Rand_col[j]] == '~')
+                                            {
+                                                Gen_board[Rand_row[i] + cell_idx, Rand_col[j]] = 'S';
+                                            }
+                                            else
+                                            {
 
-                //    }
+                                            }
+                                                break;
+                                        case 3: // UP
+                                            if (Gen_board[Rand_row[i]  ,Rand_col[j] - cell_idx] == '~')
+                                            {
+                                                Gen_board[Rand_row[i]  ,Rand_col[j] - cell_idx] = 'S';
+                                            }
+                                            else
+                                            {
 
-                //}
+                                            }
+                                                break;
+                                        case 4: // DOWN
+                                            if (Gen_board[Rand_row[i] - cell_idx, Rand_col[j] + cell_idx] == '~')
+                                            {
+                                                Gen_board[Rand_row[i] - cell_idx, Rand_col[j] + cell_idx] = 'S';
+                                            }
+                                            else
+                                            {
+
+                                            }
+                                                break;
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                }
             }
         }
     }
