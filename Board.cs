aeb463762fb4f9923cdc23c0531bbb2eca2e133d
MoroXDev <szczepanekrafal27@gmail.com>
2026-02-07 00:09:38 +0100

    Ostateczna poprawka generowania planszy

--- Board.cs
+++ Board.cs
@@ -1,29 +1,32 @@
+using System.Linq.Expressions;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 class Board
 {
     CellState[,] value = new CellState[10, 10];
-    Vector2[] index_around = new Vector2[] {
-    new Vector2(1, 0),
-    new Vector2(1, 1),
-    new Vector2(0, 1),
-    new Vector2(-1, 1),
-        new Vector2(-1, 0),
-        new Vector2(-1, -1),
-        new Vector2(0, -1),
-        new Vector2(1, -1),
+    readonly Vector2[] dirs_around = {
+        new (1, 0),
+        new (1, 1),
+        new (0, 1),
+        new (-1, 1),
+        new (-1, 0),
+        new (-1, -1),
+        new (0, -1),
+        new (1, -1),
     };
 
     public void GenerateRandom()
     {
-        int[] rand_row = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int[] rand_col = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] rand_row = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] rand_col = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         Random rand = new Random();
 
         bool ships_inserted;
         do
         {
             ships_inserted = true;
-            init();
+            restart();
             for (int ship_type = 1; ship_type < 5; ship_type++)
             {
                 for (int ships_i = 0; ships_i < ship_type; ships_i++)
@@ -31,7 +34,7 @@ class Board
                     rand.Shuffle(rand_row);
                     rand.Shuffle(rand_col);
 
-                    if (!TryAddShipRandomized(ref rand_row, ref rand_col, ship_type))
+                    if (!TryAddShipRandomized(ref rand_row, ref rand_col, ship_type, ref rand))
                     {
                         ships_inserted = false;
                         ship_type = 5; // break loop
@@ -43,13 +46,13 @@ class Board
         while (!ships_inserted);
     }
 
-    bool TryAddShipRandomized(ref int[] rand_row, ref int[] rand_col, int ship_type)
+    bool TryAddShipRandomized(ref int[] rand_row, ref int[] rand_col, int ship_type, ref Random rand)
     {
         for (int y = 0; y < 10; y++)
         {
             for (int x = 0; x < 10; x++)
             {
-                if (TryAddShipAt(rand_row[x], rand_col[y], ship_type))
+                if (TryAddShipAt(rand_row[x], rand_col[y], ship_type, ref rand))
                 {
                     return true;
                 }
@@ -58,7 +61,7 @@ class Board
         return false;
     }
 
-    bool TryAddShipAt(int x, int y, int ship_type)
+    bool TryAddShipAt(int x, int y, int ship_type, ref Random dir_rand)
     {
         if (GetCellAt(x, y) == CellState.Water)
         {
@@ -66,11 +69,13 @@ class Board
 
             Vector2[] directions =
             {
-                new Vector2(-1, 0), // LEFT
-                new Vector2(1, 0),  // RIGHT
-                new Vector2(0, -1), // UP
-                new Vector2(0, 1)   // DOWN
+                new (-1, 0), // LEFT
+                new (1, 0),  // RIGHT
+                new (0, -1), // UP
+                new (0, 1)   // DOWN
             };
+
+            dir_rand.Shuffle(directions);
 
             foreach (var dir in directions)
             {
@@ -86,20 +91,17 @@ class Board
     bool TryAddShipCells(int x, int y, int cell_count, Vector2 dir)
     {
         Vector2[] correct_indexes = new Vector2[cell_count];
+
         for (int i = 0; i < cell_count; i++)
         {
-            Vector2 next_pos = new Vector2(x + (int)dir.X * i, y + (int)dir.Y * i);
-            if (GetCellAt((int)next_pos.X, (int)next_pos.Y) == CellState.Water &&
-                CheckCellsAround((int)next_pos.X, (int)next_pos.Y, CellState.Water))
-            {
-                correct_indexes[i] = next_pos;
-            }
-            else
-            {
+            Vector2 next_pos = new(x + dir.X * i, y + dir.Y * i);
+            if (GetCellAt(next_pos) != CellState.Water || IsShipAround(next_pos))
                 return false;
-            }
+
+            correct_indexes[i] = next_pos;
         }
 
+        //dodaje statek pod koniec
         foreach (var index in correct_indexes)
         {
             value[(int)index.Y, (int)index.X] = CellState.Ship;
@@ -116,18 +118,24 @@ class Board
         return CellState.OutOfBounds;
     }
 
-    bool CheckCellsAround(int x, int y, CellState c_state)
+    CellState GetCellAt(Vector2 pos)
     {
-        if (GetCellAt(x + 1, y) == c_state ||
-            GetCellAt(x + 1, y + 1) == c_state ||
-            GetCellAt(x, y + 1) == c_state ||
-            GetCellAt(x - 1, y + 1) == c_state ||
-            GetCellAt(x - 1, y) == c_state ||
-            GetCellAt(x - 1, y - 1) == c_state ||
-            GetCellAt(x, y - 1) == c_state ||
-            GetCellAt(x + 1, y - 1) == c_state)
+        if ((int)pos.X >= 0 && pos.X < 10 && pos.Y >= 0 && pos.Y < 10)
         {
-            return true;
+            return value[(int)pos.Y, (int)pos.X];
+        }
+        return CellState.OutOfBounds;
+    }
+
+    bool IsShipAround(Vector2 pos)
+    {
+        foreach (var dir in dirs_around)
+        {
+            Vector2 index = new(pos.X + dir.X, pos.Y + dir.Y);
+            if (GetCellAt(index) == CellState.Ship)
+            {
+                return true;
+            }
         }
 
         return false;
@@ -164,7 +172,7 @@ class Board
         }
     }
 
-    void init()
+    void restart()
     {
         for (int i = 0; i < 10; i++)
         {
@@ -173,12 +181,10 @@ class Board
                 value[i, j] = CellState.Water;
             }
         }
-        // value[0, 0] = (int)CellState.Ship; //teorytycznie zmiana w X ale zmienia się w Y, pierwszy wymiar oznacza rząd a drugi oznacza kolumne
-        // value[1, 0] = (int)CellState.Ship;
     }
 
     public Board()
     {
-        init();
+        restart();
     }
 }
